4b1f703170e3a2a2b123b78967345f714c8d954a
Allan <allan.sduarte@gmail.com>
2021-10-19 09:40:14 -0300

    feat: Add Pento project

---
+++ pento/lib/pento_web/live/wrong_live.ex
@@ -0,0 +1,117 @@
+defmodule PentoWeb.WrongLive do
+  use PentoWeb, :live_view
+
+  @start_score 0
+  @start_message "Guess a number."
+  @game_over_score -3
+
+  @impl true
+  def mount(_params, _session, socket) do
+    socket =
+      socket
+      |> assign_random_number()
+      |> assign_reset_game()
+
+    {:ok, socket}
+  end
+
+  @impl true
+  def render(assigns) do
+    ~L"""
+    <h1>Your score: <%= @score %></h1>
+    <h2>
+      <%= @message %>
+      It's <%= @current_time %>
+    </h2>
+    <h2>
+      <%= for n <- 1..10 do %>
+        <a href="#" phx-click="guess" phx-value-number="<%= n %>"><%= n %></a>
+      <% end %>
+    </h2>
+
+    <%= if (@game_over == true) do %>
+    <div>
+      <%= live_patch "Game Over",
+      to: Routes.live_path(
+        @socket,
+        __MODULE__,
+        game_over: true
+      ),
+      replace: true
+      %>
+    </div>
+    <% end %>
+    """
+  end
+
+  @impl true
+  def handle_event("guess", %{"number" => guess}, %{assigns: assigns} = socket) do
+    random_number = assigns.random_number
+    guess = String.to_integer(guess)
+
+    case guess do
+      ^random_number ->
+        socket =
+          socket
+          |> assign_random_number()
+          |> assign_score("Your guess: #{guess}. Correct.", assigns.score + 1)
+
+        {:noreply, socket}
+
+      _ ->
+        socket =
+          socket
+          |> assign_random_number()
+          |> assign_score("Your guess: #{guess}. Wrong. Guess again.", assigns.score - 1)
+
+        {:noreply, socket}
+    end
+  end
+
+  @impl true
+  def handle_params(%{"game_over" => "true"}, _uri, socket) do
+    socket = assign_reset_game(socket)
+
+    {:noreply, socket}
+  end
+
+  def handle_params(_, _uri, socket) do
+    {:noreply, socket}
+  end
+
+  defp assign_reset_game(socket) do
+    assign(socket,
+      message: @start_message,
+      score: @start_score,
+      game_over: false,
+      current_time: DateTime.utc_now() |> to_string
+    )
+  end
+
+  defp assign_score(%{assigns: assigns} = socket, message, score) do
+    cond do
+      assigns[:game_over] && assigns.game_over == true ->
+        socket
+
+      true ->
+        assign(
+          socket,
+          message: message,
+          score: score,
+          current_time: DateTime.utc_now() |> to_string
+        )
+        |> maybe_assign_game_over()
+    end
+  end
+
+  defp assign_random_number(socket) do
+    random_number = Enum.random(0..10)
+    assign(socket, random_number: random_number)
+  end
+
+  defp maybe_assign_game_over(%{assigns: assigns} = socket) do
+    game_over = if assigns.score <= @game_over_score, do: true, else: false
+
+    assign(socket, game_over: game_over)
+  end
+end
