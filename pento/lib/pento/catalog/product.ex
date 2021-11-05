db9bd35989e121b4edfbc345579c582bd95595e1
Allan <allan.sduarte@gmail.com>
2021-11-05 16:34:51 -0300

    feat: Add upload

--- pento/lib/pento/catalog/product.ex
+++ pento/lib/pento/catalog/product.ex
@@ -3,10 +3,11 @@ defmodule Pento.Catalog.Product do
   import Ecto.Changeset
 
   schema "products" do
-    field(:description, :string)
-    field(:name, :string)
-    field(:sku, :integer)
-    field(:unit_price, :float)
+    field :description, :string
+    field :name, :string
+    field :sku, :integer
+    field :unit_price, :float
+    field :image_upload, :string
 
     timestamps()
   end
@@ -14,7 +15,7 @@ defmodule Pento.Catalog.Product do
   @doc false
   def changeset(product, attrs) do
     product
-    |> cast(attrs, [:name, :description, :unit_price, :sku])
+    |> cast(attrs, [:name, :description, :unit_price, :sku, :image_upload])
     |> validate_required([:name, :description, :unit_price, :sku])
     |> unique_constraint(:sku)
     |> validate_number(:unit_price, greater_than: 0.0)
