84057cdc78e39948dcf93be53153214f98cb23af
Allan <allan.sduarte@gmail.com>
2021-11-15 13:16:57 -0300

    feat: Add user live activity

--- pento/lib/pento_web/live/admin_dashboard_live.ex
+++ pento/lib/pento_web/live/admin_dashboard_live.ex
@@ -4,6 +4,7 @@ defmodule PentoWeb.AdminDashboardLive do
   def mount(_params, _session, socket) do
     {:ok,
      socket
-     |> assign(:survey_results_component_id, "survey-results")}
+     |> assign(:survey_results_component_id, "survey-results")
+     |> assign(:user_activity_component_id, "user-activity")}
   end
 end
--- pento/lib/pento_web/live/product_live/show.ex
+++ pento/lib/pento_web/live/product_live/show.ex
@@ -1,20 +1,34 @@
 defmodule PentoWeb.ProductLive.Show do
   use PentoWeb, :live_view
 
+  alias Pento.Accounts
   alias Pento.Catalog
+  alias PentoWeb.Presence
 
   @impl true
-  def mount(_params, _session, socket) do
-    {:ok, socket}
+  def mount(_params, %{"user_token" => token}, socket) do
+    {:ok, assign(socket, :user_token, token)}
   end
 
   @impl true
   def handle_params(%{"id" => id}, _, socket) do
+    product = Catalog.get_product!(id)
+    maybe_track_user(product, socket)
+
     {:noreply,
      socket
      |> assign(:page_title, page_title(socket.assigns.live_action))
-     |> assign(:product, Catalog.get_product!(id))}
+     |> assign(:product, product)}
   end
+
+  defp maybe_track_user(
+         product,
+         %{assigns: %{live_action: :show, user_token: user_token}} = socket
+       ) do
+    connected?(socket) && Presence.track_user(self(), product, user_token)
+  end
+
+  defp maybe_track_user(product, socket), do: nil
 
   defp page_title(:show), do: "Show Product"
   defp page_title(:edit), do: "Edit Product"
