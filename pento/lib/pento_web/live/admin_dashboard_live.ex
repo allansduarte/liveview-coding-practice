3c48e9b6372a880676a1622023d5749e8f164f6c
Allan <allan.sduarte@gmail.com>
2021-11-17 21:44:23 -0300

    feat: Add real time updates

--- pento/lib/pento_web/live/admin_dashboard_live.ex
+++ pento/lib/pento_web/live/admin_dashboard_live.ex
@@ -1,10 +1,30 @@
 defmodule PentoWeb.AdminDashboardLive do
   use PentoWeb, :live_view
 
+  @survey_topic "survey_results"
+  @user_activity_topic "user_activity"
+
   def mount(_params, _session, socket) do
+    connected?(socket) && do_subscribe()
+
     {:ok,
      socket
      |> assign(:survey_results_component_id, "survey-results")
      |> assign(:user_activity_component_id, "user-activity")}
+  end
+
+  @impl true
+  def handle_info(%{event: "presence_diff"}, socket) do
+    send_update(
+      UserActivityLive,
+      id: socket.assigns.user_activity_component_id
+    )
+
+    {:noreply, socket}
+  end
+
+  defp do_subscribe do
+    Endpoint.subscribe(@survey_topic)
+    Endpoint.subscribe(@user_activity_topic)
   end
 end
