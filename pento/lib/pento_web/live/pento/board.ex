7f1c92f6d5d8d6c0f8f089ab5702491f5ed4cda5
Allan <allan.sduarte@gmail.com>
2022-03-23 15:55:28 -0300

    feat: Add API

--- pento/lib/pento_web/live/pento/board.ex
+++ pento/lib/pento_web/live/pento/board.ex
@@ -5,6 +5,7 @@ defmodule PentoWeb.Pento.Board do
 
   alias PentoWeb.Pento.{Canvas, Palette, Shape}
   alias Pento.Game.{Board, Pentomino}
+  alias Pento.Game
 
   def render(assigns) do
     ~H"""
@@ -57,5 +58,52 @@ defmodule PentoWeb.Pento.Board do
 
   def assign_shapes(%{assigns: %{board: board}} = socket) do
     assign(socket, shapes: [Board.to_shape(board)])
+  end
+
+  def handle_event("pick", %{"name" => name}, socket) do
+    {:noreply, socket |> pick(name) |> assign_shapes}
+  end
+
+  def handle_event("key", %{"key" => key}, socket) do
+    {:noreply, socket |> do_key(key) |> assign_shapes}
+  end
+
+  def do_key(socket, key) do
+    case key do
+      " " -> drop(socket)
+      "ArrowLeft" -> move(socket, :left)
+      "ArrowRight" -> move(socket, :right)
+      "ArrowUp" -> move(socket, :up)
+      "ArrowDown" -> move(socket, :down)
+      "Shift" -> move(socket, :rotate)
+      "Enter" -> move(socket, :flip)
+      "Space" -> drop(socket)
+      _ -> socket
+    end
+  end
+
+  def move(socket, move) do
+    case Game.maybe_move(socket.assigns.board, move) do
+      {:error, message} ->
+        put_flash(socket, :info, message)
+
+      {:ok, board} ->
+        socket |> assign(board: board) |> assign_shapes
+    end
+  end
+
+  defp drop(socket) do
+    case Game.maybe_drop(socket.assigns.board) do
+      {:error, message} ->
+        put_flash(socket, :info, message)
+
+      {:ok, board} ->
+        socket |> assign(board: board) |> assign_shapes
+    end
+  end
+
+  defp pick(socket, name) do
+    shape_name = String.to_existing_atom(name)
+    update(socket, :board, &Board.pick(&1, shape_name))
   end
 end
